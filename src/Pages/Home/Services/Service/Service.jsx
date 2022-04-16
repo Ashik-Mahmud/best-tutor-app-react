50a23b01878b5ef4c8a75e1d0b693cc18e563136
ashikmahmud <ashikmamud187@gmail.com>
2022-04-17 02:22:02 +0600

    yeah created services sectin with responsive

---
+++ src/Pages/Home/Services/Service/Service.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { BsCheckAll } from "react-icons/bs";
+import styled from "styled-components";
+const Service = () => {
+  return (
+    <ServiceCard>
+      <div className="image">
+        <img
+          src="https://empowering-teachers.eu/wp-content/themes/yootheme/cache/home-banner-2-d0161e86.jpeg"
+          alt="img"
+        />
+      </div>
+      <div className="details">
+        <div className="inner-details">
+          <div>
+            <h3>Tech HSC student</h3>
+            <span className="colorize">400/month</span>
+          </div>
+          <div className="batch" title="Batch">
+            6th Batch
+          </div>
+        </div>
+        <div className="btn-group">
+          <button className="btn">View Details</button>
+          <button className="d-flex btn">
+            Enrolled <BsCheckAll />
+          </button>
+        </div>
+      </div>
+    </ServiceCard>
+  );
+};
+
+const ServiceCard = styled.div`
+  position: relative;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  padding: 1rem;
+  .image {
+    width: 100%;
+    height: 200px;
+    border-radius: 10px;
+    margin-bottom: 0.5rem;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .details {
+    position: relative;
+    .inner-details {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 0.4rem 0rem;
+      span {
+        font-size: 0.9rem;
+        margin: 0.2rem 0rem;
+      }
+      .batch {
+        background: #fffbf1;
+        color: var(--secondary-color);
+        display: grid;
+        place-items: center;
+        border-radius: 10%;
+        font-size: 0.8rem;
+        padding: 0.2rem 0.4rem;
+      }
+    }
+    .btn-group {
+      display: flex;
+      gap: 0.5rem;
+      margin: 0.3rem 0rem;
+      button {
+        border-radius: 4px;
+        font-size: 0.8rem;
+        padding: 0.4rem 1rem;
+        &:first-child {
+          background-color: transparent;
+          color: var(--primary-color);
+          border: 1px solid var(--primary-color);
+        }
+      }
+    }
+  }
+`;
+export default Service;
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import Service from "./Service/Service";
+
+const Services = () => {
+  return (
+    <section id="services">
+      <ServicesContainer>
+        <div className="container">
+          <div className="title">
+            <h2>What's done?</h2>
+            <span>get your suitable packages and enrolled.</span>
+          </div>
+          <div className="services-content">
+            <Service />
+            <Service />
+            <Service />
+          </div>
+        </div>
+      </ServicesContainer>
+    </section>
+  );
+};
+const ServicesContainer = styled.section`
+  position: relative;
+  padding: 3rem 0rem;
+  .title {
+    margin: 2rem 0rem;
+    span {
+      font-size: 1rem;
+      color: #555;
+    }
+  }
+  .services-content {
+    margin: 2rem 0rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+    grid-gap: 2rem;
+    @media (max-width: 768px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media (max-width: 590px) {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
+`;
+export default Services;
